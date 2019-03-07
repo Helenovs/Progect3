f187a375aa6f9339e99757792d8983749721071f
Elena Ovsyannikova <helenovsau@gmail.com>
2019-03-07 14:39:13 +1100

    Updates

---
+++ precache-manifest.9a1450398fa2c7ee4d24f760823d8558.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "17f293f286b7c3844366",
+    "url": "/Progect3/static/css/main.3316efae.chunk.css"
+  },
+  {
+    "revision": "17f293f286b7c3844366",
+    "url": "/Progect3/static/js/main.17f293f2.chunk.js"
+  },
+  {
+    "revision": "773372ae79ad992ca833",
+    "url": "/Progect3/static/js/runtime~main.773372ae.js"
+  },
+  {
+    "revision": "93df127907be92215b99",
+    "url": "/Progect3/static/js/2.93df1279.chunk.js"
+  },
+  {
+    "revision": "a2be94b7aee3de8bf414a8745c038209",
+    "url": "/Progect3/index.html"
+  }
+];
